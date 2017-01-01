b977d183f963108cdf3f43007e1f12fc7ac18834
buzzed1 <smitstephen@gmail.com>
2016-12-31 23:34:17 -0500

    Added files
    Added files to repository.

---
+++ index.php
@@ -0,0 +1,55 @@
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	
+		<body>
+			<nav>
+				<h1 id="logo-head">Stephen Smith Photography</h1>
+				
+					<div id="nav-bar">
+						<ul>
+							<li>Weddings</li>
+							<li>Portraits</li>
+							<li>Architectural</li>
+							<li>About</li>
+						</ul>
+					</div><!-- nav bar -->
+			</nav><!-- nav -->
+			<div class="content">
+				<div id="fold">
+					<img src="#">
+					<h2>Welcome to Stephen Smith Photography</h2>
+					
+					<p>We believe in capturing the moment that means the most to you.</p>
+							
+				</div><!-- fold -->
+				
+				<div id="photos-grid">
+					
+				</div><!-- photo-grid -->
+				
+					<div id="inform">
+					
+					</div><!-- inform -->
+				</div><!-- content -->
+				
+					<div id="footer">
+						<div id="address">
+							<p>Stephen Smith Photography</p>
+							<p>1-876-401-0635</p>
+							<p>Westgreen</p>
+							<p>Montego Bay</p>
+							<p>St James</p>
+							
+						</div><!-- address -->
+				
+				</div><!-- footer -->
+		</body><!-- body -->
+
+
+</html>
